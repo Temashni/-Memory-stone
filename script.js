95d791b71ab2634fda85fb0b01f5b4edbd33453a
Artem Mishin <artem.mishinbet98@mail.ru>
2021-05-20 17:25:13 +0400

    About-is-adaptive

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ function updateMargin() {
     document.getElementById("manager-slide")
         .style.marginLeft = indexActiveSlide * 600 * -1 + 'px';
 }
+
+function show(){ document.getElementById("modal").style.display="flex";
+document.getElementById("scroll").style.overflow="hidden";
+}
+
+function unshow(){
+  document.getElementById("modal").style.display="none";
+  document.getElementById("scroll").style.overflow="auto";
+}
+
